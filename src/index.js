ba2a223a052067d7e7a32d063effb815b300f304
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-06-24 16:22:47 +0200

    additional changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Card.js';
+import 'tachyons';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Card/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
